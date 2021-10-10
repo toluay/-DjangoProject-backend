dcc6ce297d7fc9a146294b27bd52778a9c9f2adb
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-10-10 17:28:59 +0100

    knox migration done and accounts app created

--- customerbaseappapi/customerbaseappapi/settings.py
+++ customerbaseappapi/customerbaseappapi/settings.py
@@ -39,7 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'customers',
+     'knox',
+    'accounts',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
