c73fc9b083c00f2fd4b1dbbc64a35bea0efbbe19
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-10-10 17:20:53 +0100

    customer api changed to only get response to authenticated user

--- customerbaseappapi/customers/api.py
+++ customerbaseappapi/customers/api.py
@@ -6,9 +6,16 @@ from .serializers import CustomerSerializer
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
-    queryset = Customer.objects.all()
+
     permission_classes = [
         permissions.AllowAny,
     ]
     serializer_class = CustomerSerializer
+
+    def get_queryset(self):
+        return self.request.user.customers.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
